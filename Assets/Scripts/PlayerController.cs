ff23d20a599af87809aaff669df53f192096769f
DESKTOP-9OJMA0V\pronick <pronick@gmail.com>
2022-09-13 00:11:55 +0200

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,22 +10,28 @@ public class PlayerController : MonoBehaviour
     public bool alive;
     public bool firstClick = false;
     public static bool shield = false;
+    private static bool vulnerable = true;
 
 
     //audio
     public AudioClip crashPlane;
     public AudioClip audioPlane;
+    public AudioClip powerPlane;
     public AudioSource audioPlayer;
 
     public AudioClip coinSound;
     public AudioSource coinPlayer;
-    public float playerVolume;
+    private float playerVolume = 0.05f;
+    private float coinVolume = 0.1f;
+    private float powerVolume = 0.3f;
 
     public SceneController sceneController;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.GetComponent<Animator>().SetBool("crashed", false);
         shield = false;
         maxRotate = 0f;
         //scene set
@@ -39,6 +45,7 @@ public class PlayerController : MonoBehaviour
             audioPlayer.clip = audioPlane;
 
             coinPlayer.Stop();
+            coinPlayer.volume = coinVolume;
             coinPlayer.clip = coinSound;
 
         }
@@ -55,11 +62,7 @@ public class PlayerController : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 // Check if the mouse was clicked over a UI element
-                if (EventSystem.current.IsPointerOverGameObject() && EventSystem.current.currentSelectedGameObject?.gameObject.tag == "Settings")
-                {
-
-                }
-                else
+                if (!EventSystem.current.IsPointerOverGameObject() || EventSystem.current.currentSelectedGameObject?.gameObject.tag != "Settings")
                 {
                     if (Time.timeScale > 0)
                     {
@@ -69,15 +72,12 @@ public class PlayerController : MonoBehaviour
                             GetComponent<Rigidbody2D>().gravityScale = 0.3f;
                             StartCoroutine(HideClickButton());
                         }
-                        if (sceneController.GetSoundValue())
+                        if (sceneController.GetSoundValue() && vulnerable)
                         {
                             audioPlayer.Stop();
                             audioPlayer.Play();
                         }
-                        //playerRB.velocity = Vector2.zero;
-                        //playerRB.AddForce(new Vector2(0, 200f));
                         playerRB.velocity = Vector2.up * speed;
-                        //maxRotate = 40 - transform.localEulerAngles.z;
                     }
                 }
 
@@ -86,8 +86,6 @@ public class PlayerController : MonoBehaviour
             //Rotations
             if (firstClick && Time.timeScale > 0)
             {
-                //Debug.Log("primero rot: " + transform.rotation);
-                //Debug.Log("primero localEulerAngles: " + transform.localEulerAngles.z);
                 if (maxRotate < 0 && playerRB.velocity.y > 0)
                 {
                     transform.rotation = Quaternion.Euler(0, 0, 0f);
@@ -111,9 +109,9 @@ public class PlayerController : MonoBehaviour
     //power controller
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Power")
+        if (collision.gameObject.tag == "Power" && alive)
         {
-            //score point collision
+            //power objects collision
             switch (collision.gameObject.name)
             {
                 case "Diamond":
@@ -137,32 +135,94 @@ public class PlayerController : MonoBehaviour
                     shield = true;
                     sceneController.ToggleShield(true);
                     break;
-
-
             }
             collision.gameObject.SetActive(false);
-            coinPlayer.Play();
+            if (sceneController.GetSoundValue())
+            {
+                coinPlayer.Play();
+            }
         }
-        else if (collision.gameObject.tag == "FlyEnemy")
+        else if (collision.gameObject.tag == "FlyEnemy" || collision.gameObject.tag == "Enemy")
         {
-            //enemy collision
+            //enemy collision check
+            checkIsVulnerable();
+        }
+        else if (collision.gameObject.tag == "Floor" && alive)
+        {
+            //enemy collision with the floor always lose
             loseGame();
         }
+        else if (collision.gameObject.tag == "Score" && alive)
+        {
+            //prevent double score disabling score line
+            collision.gameObject.SetActive(false);
+            ScoreController.score++;
+        }
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void checkIsVulnerable()
     {
-        loseGame();
+        //if have shield
+        if (!shield)
+        {
+            if (vulnerable && alive)
+            {
+                loseGame();
+            }
+        }
+        else
+        {
+            //disable damages
+            vulnerable = false;
+            //if don't have
+            shield = false;
+            //hide shiel icon
+            sceneController.ToggleShield(false);
+            //start count down to reactive damage
+            StartCoroutine(invulnerableTime());
+            //set animator variable to true for star power animation
+            gameObject.GetComponent<Animator>().SetBool("playerPower", true);
+        }
     }
+
+    private IEnumerator invulnerableTime()
+    {
+        // audio
+        audioPlayer.Stop();
+        audioPlayer.volume = powerVolume;
+        audioPlayer.clip = powerPlane;
+        if (sceneController.GetSoundValue())
+        {
+            audioPlayer.Play();
+        }
+
+        // count down to reactiave damages
+        yield return new WaitForSeconds(5);
+        if (!vulnerable)
+        {
+            //audio
+            audioPlayer.Stop();
+            audioPlayer.volume = playerVolume;
+            audioPlayer.clip = audioPlane;
+
+            vulnerable = true;
+            gameObject.GetComponent<Animator>().SetBool("playerPower", false);
+        }
+
+    }
+
 
     private void loseGame()
     {
+        alive = false;
+        gameObject.GetComponent<Animator>().SetBool("crashed", true);
         //audio
+        audioPlayer.Stop();
+        audioPlayer.volume = powerVolume;
+        audioPlayer.clip = crashPlane;
         if (sceneController.GetSoundValue())
         {
-            audioPlayer.Stop();
-            audioPlayer.clip = crashPlane;
             audioPlayer.Play();
         }
         alive = false;
@@ -172,10 +232,5 @@ public class PlayerController : MonoBehaviour
     public bool GetFirstClick()
     {
         return firstClick;
-    }
-
-    private void SetPlayerAlpha(string name, bool value)
-    {
-        //gameObject.GetComponent<Image>().color = value ? solid : transp;
     }
 }
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -12,67 +12,81 @@ public class SceneController : MonoBehaviour
     public GameObject loseGO;
     public GameObject settingsGO;
     public GameObject scoreGO;
-    //private GameObject scoreGO;
-    //private GameObject recordTextGO;
+    public GameObject generatorController;
     public AudioClip audioGame;
     public AudioClip audioLose;
     public AudioSource music;
     public float musicVolume;
     public GameObject player;
     private GameObject playerInstace;
-    private bool musicOn = true;
-    private bool soundOn = true;
+    private bool musicOn;
+    private bool soundOn;
 
     private Color transp = new Color(255, 255, 255, 0.5f);
     private Color solid = new Color(255, 255, 255, 1);
-
-
-
+    private GameObject resetButton;
+    private GameObject pauseButton;
+    private GameObject clickImage;
+    private GameObject shieldButton;
+    private GameObject playButton;
 
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0;
+
+        // music and sound settings
+        musicOn = !PlayerPrefs.HasKey("music") || PlayerPrefs.GetInt("music") == 1;
+        soundOn = !PlayerPrefs.HasKey("sound") || PlayerPrefs.GetInt("sound") == 1;
         music.clip = audioLose;
-        music.Play();
+        if (musicOn)
+        {
+            music.Play();
+        }
+
+        // score setting
         GameObject.Find("RecordText").GetComponent<TMPro.TextMeshProUGUI>().text = "RECORD: " + PlayerPrefs.GetInt("record", 0).ToString();
+        
+        // buttons settings
+        resetButton = loseGO.transform.Find("ResetButton").gameObject;
+        pauseButton = settingsGO.transform.Find("PauseButton").gameObject;
+        clickImage = scoreGO.transform.Find("ClickImage").gameObject;
+        shieldButton = settingsGO.transform.Find("ShieldButton").gameObject;
+        playButton = loseGO.transform.Find("PlayButton").gameObject;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
 
     public void RunGame()
     {
-
-        settingsGO.transform.Find("PauseButton").gameObject.SetActive(true);
-
-        //SetButtonAlpha("MusicButton", true);
-
+        // instant score reset
         ScoreController.score = 0;
+
+        // buttons reset
+        pauseButton.SetActive(true);
+        resetButton.SetActive(false);
         playerInstace = Instantiate(player, new Vector3(-0.40f, 0, 0), Quaternion.identity);
         loseGO.SetActive(false);
-        scoreGO.transform.Find("ClickImage").gameObject.SetActive(true);
-        Time.timeScale = 1;
+        clickImage.SetActive(true);
+
+        // set the music play
         music.Stop();
         music.clip = audioGame;
         if (musicOn)
         {
             music.Play();
         }
+
+        // start game
+        Time.timeScale = 1;
     }
 
     public void ToggleShield(bool value)
     {
-        settingsGO.transform.Find("ShieldButton").gameObject.SetActive(value);
+        shieldButton.SetActive(value);
     }
 
     public void Lose()
     {
-        settingsGO.transform.Find("PauseButton").gameObject.SetActive(false);
+        pauseButton.SetActive(false);
         music.Stop();
         music.clip = audioLose;
         if (musicOn)
@@ -84,17 +98,25 @@ public class SceneController : MonoBehaviour
             PlayerPrefs.SetInt("record", ScoreController.score);
         }
         loseGO.SetActive(true);
-        loseGO.transform.Find("PlayButton").gameObject.SetActive(false);
-        loseGO.transform.Find("ResetButton").gameObject.SetActive(true);
+        playButton.SetActive(false);
         // Get child nested parent, parsing to GO
         loseGO.transform.Find("GameOver").gameObject.SetActive(true);
         GameObject.Find("RecordText").GetComponent<TMPro.TextMeshProUGUI>().text = "RECORD: " + PlayerPrefs.GetInt("record", 0).ToString();
-        Time.timeScale = 0;
+        StartCoroutine(stopTimeScale());
 
+    }
+
+    private IEnumerator stopTimeScale()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Time.timeScale = 0;
+        resetButton.SetActive(true);
+        Debug.Log("parando el juego");
     }
 
     public void Reset()
     {
+        generatorController.GetComponent<GeneratorController>().initGenerator();
         removeEnemies();
         removePowers();
         Destroy(playerInstace);
@@ -134,12 +156,14 @@ public class SceneController : MonoBehaviour
             musicOn = false;
             music.Stop();
             SetButtonAlpha("MusicButton", false);
+            PlayerPrefs.SetInt("music", 0);
         }
         else
         {
             musicOn = true;
             music.Play();
             SetButtonAlpha("MusicButton", true);
+            PlayerPrefs.SetInt("music", 1);
         }
     }
 
@@ -157,6 +181,7 @@ public class SceneController : MonoBehaviour
     {
         soundOn = !soundOn;
         SetButtonAlpha("SoundButton", soundOn);
+        PlayerPrefs.SetInt("sound", soundOn ? 1 : 0);
     }
 
     public void PauseGame()
@@ -173,13 +198,14 @@ public class SceneController : MonoBehaviour
 
     public void Exit()
     {
-        if (UnityEditor.EditorApplication.isPlaying)
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
-        else
-        {
-            Application.Quit();
-        }
+        Application.Quit();
+        //if (UnityEditor.EditorApplication.isPlaying)
+        //{
+        //    UnityEditor.EditorApplication.isPlaying = false;
+        //}
+        //else
+        //{
+        //    Application.Quit();
+        //}
     }
 }
